c5d6521cb40120e518e0f46fdd62853464208b86
Michał Połosak <mpolosak@op.pl>
2025-03-19 17:58:45 +0100

    Add class Container

---
+++ APBD3/Container.cs
@@ -0,0 +1,21 @@
+﻿namespace APBD3;
+
+public abstract class Container
+{
+    public double MassOfLoad { get; set; }
+    public double Height { get; set; }
+    public double OwnMass { get; set; }
+    public double Depth { get; set; }
+    public string SerialNumber { get; set; } = string.Empty;
+    public double Capacity { get; set; }
+    
+    public void Empty() => MassOfLoad = 0;
+    public void Load(double mass)
+    {
+        if (mass > Capacity)
+        {
+            throw new Exception("Capacity exceeded");
+        }
+        MassOfLoad = mass;
+    }
+}
